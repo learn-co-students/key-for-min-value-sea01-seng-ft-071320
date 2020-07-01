c8bc4c8e4996e75e5612e85c418f3ddfe705cad8
Brad Beggs <brad.beggs@gmail.com>
2020-07-01 04:31:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 holding_value = nil   #empty var to initialize
-holding_name_key = nil #empty var to initalize to available in func block
+holding_name_key = nil #empty var to initalize so as to be available in method scope
 
   name_hash.each do |key, value|
     if holding_value == nil
